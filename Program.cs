248ee80983545382a87c2be108fc04273103e6fa
Silktz <caua.franco3@etec.sp.gov.br>
2023-05-12 22:22:20 -0300

    indo

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Made by Silktz 
+
+int qtdkid,qtdadult,qtdAdultND;
+
+double carne,cerveja,acomp,soda,water; 
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("=-=-=-=-=-CHURRASCO DO NORTE-=-=-=-=-=");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.Write("adultos que consomem bebidas alcoólicas: ");
+Console.ResetColor();
+
+onsole.ForegroundColor = ConsoleColor.DarkRed;
+Console.Write("adultos que não consomem bebidas alcoólicas: ");
+Console.ResetColor();
+
+
+qtdAdultND = Convert.ToInt32(Console.ReadLine());
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine('Crianças: ');
+Console.ResetColor();
+
+
+
+
