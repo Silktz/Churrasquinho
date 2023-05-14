c5682140e2b93ba2c66210d7b84e35f5ced0c386
Silktz <caua.franco3@etec.sp.gov.br>
2023-05-14 19:20:26 -0300

    finalizado

--- Program.cs
+++ Program.cs
@@ -12,18 +12,47 @@ Console.ForegroundColor = ConsoleColor.DarkGreen;
 Console.Write("adultos que consomem bebidas alcoólicas: ");
 Console.ResetColor();
 
-onsole.ForegroundColor = ConsoleColor.DarkRed;
-Console.Write("adultos que não consomem bebidas alcoólicas: ");
+qtdadult = Convert.ToInt32(Console.ReadLine());
 Console.ResetColor();
 
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.Write("adultos que não consomem bebidas alcoólicas: ");
+Console.ResetColor();
 
 qtdAdultND = Convert.ToInt32(Console.ReadLine());
 Console.ResetColor();
 
 Console.ForegroundColor = ConsoleColor.DarkYellow;
-Console.WriteLine('Crianças: ');
+Console.WriteLine("Criancas: ");
 Console.ResetColor();
 
+qtdkid = Convert.ToInt32(Console.ReadLine());
 
 
 
+int ALL_ADULTS = qtdAdultND + qtdadult;
+carne = 0.4 * ALL_ADULTS + 0.2 * qtdkid;
+
+int ALL = ALL_ADULTS + qtdkid;
+acomp = 0.2 * ALL;
+
+cerveja = 2 * qtdadult;
+
+int AlcoolNO = qtdAdultND + qtdkid;
+soda = 0.5 * AlcoolNO;
+
+water = 0.4 * ALL;
+
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("=-=-=-RESULTADO-=-=-=-");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine($"carnes : {carne:N1} kg");
+Console.WriteLine($"acompanhamentos : {acomp:N1} kg");
+Console.WriteLine($"cervejas : {cerveja:N1} L");
+Console.WriteLine($"refrigerantes : {soda:N1} L");
+Console.WriteLine($"águas : {water:N1} L");
+Console.ResetColor();
